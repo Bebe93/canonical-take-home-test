cab57fa19e98c5c22d09db5124a0bb9874939764
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-06-11 11:04:56 +0200

    setup

---
+++ canonical-test/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React, {useState} from React;
+
+function App() {
+const [artile, setArticle] = useState()
+
+
+
+
+  return <div className="App"></div>;
+}
+
+export default App;
