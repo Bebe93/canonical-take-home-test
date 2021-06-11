f8c17215496121d3eded0387f5eee5b743e61e95
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-06-11 16:13:25 +0200

    comments

--- canonical-test/src/App.js
+++ canonical-test/src/App.js
@@ -1,6 +1,5 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "axios"; // I used axios instead of fetch for no particular reason other than avoiding one line more of code such as .then(res => res.json())
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -27,6 +26,7 @@ function App() {
               <div key={article.id}>
                 <ul className="p-list row">
                   <li className="p-list__item col-4 p-card--highlighted ">
+                    {/* I wasn't able to add the first line of colour, I tried with notification but eventually realised that it wasn't the right option */}
                     <h1 className="p-card__title p-text--x-small-capitalised ">
                       CLOUD AND SERVER
                     </h1>
@@ -38,7 +38,7 @@ function App() {
                     </a>
                     <br />
                     <p>
-                      By{" "}
+                      By
                       <a
                         href={article._embedded.author[0].link}
                         target="_blank"
@@ -48,7 +48,9 @@ function App() {
                       </a>{" "}
                       on {article.date}
                     </p>
+                    {/* I wasn't ale to change the format of the date */}
                     <hr />
+                    {/* I didn't know how to choose a dotted separator */}
                     <p className="p-text--small">Article</p>
                   </li>
                 </ul>
